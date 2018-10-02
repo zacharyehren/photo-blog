65439161b265da32debc93ab94b4d49e633dfe1b
Zachary Ehren <zachehren@gmail.com>
2018-10-01 19:18:42 -0700

    style entry template, add like button and comments

--- src/app/entries/entry/entry.component.ts
+++ src/app/entries/entry/entry.component.ts
@@ -10,4 +10,9 @@ export class EntryComponent {
     title: string = 'My First Photo';
     photo: string = 'http://placehold.it/800x500?text=Angular Basics';
     description: string = 'A description of My First Photo';
+    comments: any[] = [
+        {name: "John", comment: "A comment"},
+        {name: "Jim", comment: "A comment"},
+        {name: "Jen", comment: "A comment"}
+    ]
 }
