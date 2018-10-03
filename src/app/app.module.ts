d39d3dfe1e2024e09d3f7b910064781ea891ca26
Zachary Ehren <zachehren@gmail.com>
2018-10-02 17:55:41 -0700

    create comment form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { NgModule } from '@angular/core';
 // package for browser specific features
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 // any time a new component is created, it needs to be imported to app.module (similar to adding a src tag to the index.html file)
 import { AppComponent } from './app.component';
-import { EntryListComponent, EntryComponent, EntryService } from './entries'
+import { EntryListComponent, EntryComponent, EntryService, EntryCommentFormComponent } from './entries'
 import { InMemoryEntryService } from './backend';
 
 @NgModule({
@@ -18,6 +19,7 @@ import { InMemoryEntryService } from './backend';
 // angular now knows that the app will be used in a web browser
         BrowserModule,
         HttpModule,
+        FormsModule,
         InMemoryWebApiModule.forRoot(InMemoryEntryService)   
     ],
     // providers array lets the angular know what services are availabe when compiled (also allows the @injectable decorator to work)
@@ -27,7 +29,8 @@ import { InMemoryEntryService } from './backend';
     declarations: [
         AppComponent,
         EntryComponent, 
-        EntryListComponent
+        EntryListComponent,
+        EntryCommentFormComponent
     ],
     // collection of top level components that act as an entry point or root of our application (generally one per application)
     bootstrap: [AppComponent]
--- src/vendor.ts
+++ src/vendor.ts
@@ -3,7 +3,8 @@ import '@angular/core';
 import '@angular/common';
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
-import '@angular/http'
+import '@angular/http';
+import '@angular/forms';
 
 // 3rd party
 import 'core-js';
