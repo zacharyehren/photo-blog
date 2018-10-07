5b25260b9bde9a001b51a4807f459663824450b2
Zachary Ehren <zachehren@gmail.com>
2018-10-07 16:22:51 -0700

    reset form after comment is submitted

--- src/app/entries/entry/entry-comment-form.component.ts
+++ src/app/entries/entry/entry-comment-form.component.ts
@@ -1,6 +1,8 @@
 // EventEmitter is a way to pass data between two components
 // The Output decorator allows us send data out to the parent
-import { Component, EventEmitter, Output } from '@angular/core';
+// ViewChild lets the component look for local variables in the template
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-entry-comment-form',
@@ -12,9 +14,12 @@ export class EntryCommentFormComponent {
     name: string = "";
     comment: string = "";
     @Output() onCommentAdded = new EventEmitter<{name: string; comment: string;}>();
-    onSubmit(){
+    // binds the local variable to the commentForm property declared here
+    @ViewChild('commentForm') commentForm: NgForm;
+    onSubmit(commentForm: NgForm){
         let comment = { name: this.name, comment: this.comment };
         // passes the comment object to the onCommentAdded function
         this.onCommentAdded.emit(comment);
+        this.commentForm.resetForm();
     }
 }
